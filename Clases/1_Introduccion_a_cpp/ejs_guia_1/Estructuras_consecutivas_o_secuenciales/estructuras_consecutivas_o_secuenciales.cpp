a2e78a861ef336bc3b97f6cf6502649adf7d83a3
germandus <germandouce@gmail.com>
2021-09-10 20:52:59 -0300

    fin seccion 1 ejs guia 1

---
+++ Clases/1_Introduccion_a_cpp/ejs_guia_1/Estructuras_consecutivas_o_secuenciales/estructuras_consecutivas_o_secuenciales.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+const double PI = 3.141592653589793;
+
+
+/*1)
+Leer un número por teclado e imprimirlo en pantalla con el siguiente 
+cartel: “Numero ingresado" = numero.
+*/
+
+// int main(){
+//   int numero;
+//   cin >> numero;
+//   cout << "Numero ingresado: " << numero; 
+// }
+
+
+
+/*2)
+Leer dos números por teclado e imprimir:
+-La suma de ambos
+-La resta (el primero menos el segundo)
+-La multiplicación
+-La división 
+*/
+
+// int main(){
+//   int numero_1, numero_2;
+//   cout << "Numero 1: ";
+//   cin >> numero_1;
+  
+//   cout << "Numero 2: ";
+//   cin >> numero_2;
+
+//   cout << "Suma: " << numero_1 + numero_2 << endl; 
+//   cout << "Resta: " << numero_1 - numero_2 << endl; 
+//   cout << "Multiplicacion: " << numero_1 * numero_2 << endl; 
+//   cout << "Division: " << (float)numero_1 / numero_2 << endl; 
+// }
+
+
+
+/*3)
+Escribir un programa que lea el nombre de una persona y luego lo salude. 
+*/
+
+// int main(){
+//   cout << "Ingrese su nombre: ";
+//   string nombre;
+//   cin >> nombre;
+//   cout << "Hola " << nombre <<" !!";
+// }
+
+
+
+/*4)
+Dado el radio R de una esfera que se solicita por teclado, calcular e imprimir su 
+superficie y su volumen.
+*/
+// int main(){
+//   cout << "Ingrese el radio de la esfera: ";
+//   int radio;
+//   cin >> radio;
+  
+//   cout << "Superficie esfera = " << PI*(pow(radio,2)) << endl;
+  
+//   double superficie = (4/3)*PI*(pow(radio,3));
+//   cout << "Volumen esfera = " << superficie;
+
+// }
+
+
+
+/*5)
+Leer la base y la altura de un rectángulo, calcular el perímetro y la superficie.
+*/
+
+// int main(){
+//   float base, altura;
+//   cout << "Ingrese la base del rectangulo: ";
+//   cin >> base;
+
+//   cout << "Ingrese la altura del rectangulo: ";
+//   cin >> altura;
+  
+//   cout << "Superficie triangulo = " << (base*altura)/2 << endl;
+//   //cout << "Perimetro triangulo = " << superficie; muy largo!
+
+// }
+
