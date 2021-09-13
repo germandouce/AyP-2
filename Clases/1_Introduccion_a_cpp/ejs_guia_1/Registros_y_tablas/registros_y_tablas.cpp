884bafa43e54cb8ac56f4a4084ebabbf31af2ec4
germandus <germandouce@gmail.com>
2021-09-12 22:46:46 -0300

    agrego ej 40 seccion registros y tablas

---
+++ Clases/1_Introduccion_a_cpp/ejs_guia_1/Registros_y_tablas/registros_y_tablas.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SALIR = 0;
+const int MAX_PERS = 10;
+/*40)
+Definir un struct Persona, donde se pueda almacenar su nombre, y el número de teléfono.
+*/
+
+// struct Persona {
+//     string nombre;
+//     long numero;
+// };
+
+// int main(){
+//     int opc = 1;
+//     Persona personas[MAX_PERS];
+//     int i = 0;
+//     while (opc!=SALIR){
+//         Persona nueva_persona;
+
+//         cout <<"Ingrese nombre: ";
+//         cin >> nueva_persona.nombre;
+        
+//         cout<<"Ingrese numero de " << nueva_persona.nombre <<" : ";
+//         cin>> nueva_persona.numero;
+
+//         personas[i] = nueva_persona;
+        
+//         cout<<"Nueva persona: 1-Si 0-No: ";
+//         cin >> opc;
+//         i++;
+//     };
+//     cout <<"---personas ingresadas---"<<endl;
+//     for (int j = 0; j<i; j++){
+//         cout <<"nombre: " << personas[j].nombre <<endl;
+//         cout <<"numero: " << personas[j].numero<<endl;
+//         cout <<"------------"<<endl;
+
+//     }
+
+
+// }
