0ba6d94a1a70d0dd4601691e7edd7d37bce9d29b
German Douce <germandouce@gmail.com>
2021-10-19 17:21:45 -0300

    se agregan varios archivos

--- Clases/3_POO/templates/EjercicioClasesTemplates/Operaciones.cpp
+++ Clases/3_POO/templates/EjercicioClasesTemplates/Operaciones.cpp
@@ -1,19 +1,29 @@
-#include "Operaciones.h"
+//Este archivo no es necesario!!
 
-template < class Tipo >
-int Operaciones<Tipo>::sumar(Tipo n1, Tipo n2){
-    return n1 + n2;
-}
+// Cuando se define una clase template en este caso clase "operaciones"
+// de tipo "Tipo" toda la implementacion del template se escribe en el .h
+//Ademas, recordar que se debe escribir 
+//template <class tipo>
+//antes de cada metodo que implemente
+//Se sigue manteniendo la estructura de, definicion primero, segundo implmentacion
+//pero TODO en el .h
 
-template < class Tipo2 >
-Tipo2 Operaciones<Tipo>::restar(Tipo2 un_numero, Tipo2 otroNumero){
-    return un_numero - otroNumero;
-}
+// #include "Operaciones.h"
 
-template < class Tipo >
-Tipo Operaciones::multiplicar(Tipo aNumber, Tipo anotherNumber) {return aNumber ** anotherNumber;}
+// template < class Tipo >
+// int Operaciones<Tipo>::sumar(Tipo n1, Tipo n2){
+//     return n1 + n2;
+// }
 
+// template < class Tipo >
+// Tipo Operaciones<Tipo>::restar(Tipo un_numero, Tipo otroNumero){
+//     return un_numero - otroNumero;
+// }
 
-Tipo Operaciones<Tipo>::dividir(Tipo n1,Tipo n2) {
-    return n1/n2;
-}
+// template < class Tipo >
+// Tipo Operaciones<Tipo>::multiplicar(Tipo aNumber, Tipo anotherNumber) {return aNumber ** anotherNumber;}
+
+// template < class Tipo >
+// Tipo Operaciones<Tipo>::dividir(Tipo n1,Tipo n2) {
+//     return n1/n2;
+// }
--- Clases/3_POO/templates/EjercicioClasesTemplates/Operaciones.h
+++ Clases/3_POO/templates/EjercicioClasesTemplates/Operaciones.h
@@ -4,11 +4,28 @@
 template < typename Tipo >
 class Operaciones {
 
-private:
+public:
     int sumar(Tipo n1, Tipo n2);
-    Tipo2 restar(Tipo2 un_numero, Tipo2 otroNumero);
+    Tipo restar(Tipo un_numero, Tipo otroNumero);
     Tipo dividir(Tipo aNumber, Tipo anotherNumber);
     Tipo multiplicar(Tipo n1, Tipo n2);
 
+    template < class Tipo >
+    int Operaciones<Tipo>::sumar(Tipo n1, Tipo n2){
+        return n1 + n2;
+    }
+
+    template < class Tipo >
+    Tipo Operaciones<Tipo>::restar(Tipo un_numero, Tipo otroNumero){
+        return un_numero - otroNumero;
+    }
+
+    template < class Tipo >
+    Tipo Operaciones<Tipo>::multiplicar(Tipo aNumber, Tipo anotherNumber) {return aNumber ** anotherNumber;}
+
+    template < class Tipo >
+    Tipo Operaciones<Tipo>::dividir(Tipo n1,Tipo n2) {
+        return n1/n2;
+    }
 };
 #endif //UNTITLED_OPERACIONES_H
